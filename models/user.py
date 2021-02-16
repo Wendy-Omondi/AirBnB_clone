d0bb9f7563c81d3382ef1ed13cb82037d98b50bd
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-02-16 22:37:05 +0000

    first user commit

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a class user that inherits from BaseModel"""
+
+
+class User:
+    """user details"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
