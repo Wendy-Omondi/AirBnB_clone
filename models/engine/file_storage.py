fd47ae6dd079b18e50fab2a189b5a30eeed82cff
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-02-16 20:41:04 +0000

    storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import json
+from models.base_model import BaseModel
+
 class FileStorage:
     """serializes instances to a JSON file and deserializes JSON file to instances"""
 
@@ -6,12 +9,29 @@ class FileStorage:
     def __init__(self):
         """Initializes"""
         self.__file_path = str(file.json)
-        self.__objects = pass
+        self.__objects = {}
 
     def all(self):
         """returns the dictionary __objects"""
-        return __objects.__dict__
+        
+    return __objects.__dict__
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        
+        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
+        obj = json.dumps(self, default=self.__objects)
+        return obj
+    def reload(self):
+        """
+        deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists
+        ; otherwise, do nothing. If the file doesn’t exist,
+        no exception should be raised)
+        """
+        try:
+            self.__object = json.load(self.__file_path)
+        except:
+            pass
