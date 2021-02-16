296ae08921a998ae509f4e4a9af0e8d2f7a7e6fb
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-02-16 23:04:53 +0000

    user

--- models/user.py
+++ models/user.py
@@ -6,10 +6,7 @@ from models.base_model import BaseModel
 
 class User:
     """user details"""
-
-    def __init__(def):
-        """user dtails"""
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
