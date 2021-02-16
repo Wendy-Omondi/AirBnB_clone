147b44faad31567110638336a6e80571ea667bcd
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-02-16 22:59:48 +0000

    state first commit

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defines the state class"""
+
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """states search"""
+
+    def __init__(self):
+        """initializes"""
+        name = ""
+    self.name = name
