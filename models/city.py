b0dc0128584e6df256ebfa13b2cc3a637f66fdd5
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-02-17 15:08:51 +0000

    city first commit

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defines a city class"""
+
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """describes city parameters"""
+
+    state_id = ""
+    name = ""
