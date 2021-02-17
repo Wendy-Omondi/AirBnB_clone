5ce8b2d47384a5237c72a30112dd02559de60b6f
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-02-17 20:20:12 +0000

    WIP

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,16 +16,19 @@ class FileStorage:
     def all(self):
         """returns the dictionary __objects"""
 
-        return __objects.__dict__
+        return FileStorage.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+        o_class_name = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        obj = json.dumps(self, default=self.__objects)
-        return obj
+        o_d = FileStorage.__objects
+        o_dic = {obj: o_d[obj].to_dict() for obj in o_d.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(o_dic, f)
 
     def reload(self):
         """
@@ -35,6 +38,11 @@ class FileStorage:
         no exception should be raised)
         """
         try:
-            self.__object = json.load(self.__file_path)
-        except:
+            with open(FileStorage.__file_path) as f:
+                o_dic = json.load(f)
+                for o in o_dic.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
             pass
