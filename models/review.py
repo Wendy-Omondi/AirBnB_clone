d0de0674f698e33f89790ce2ca832c2dbf0cda09
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-02-17 15:15:28 +0000

    review first commit

---
+++ models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defines a review class"""
+
+
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """review details"""
+    place_id = ""
+    user_id = ""
+    text = ""
