e887a7ec2fe29bb3d7daa032c858643b27468a6f
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-02-17 15:39:17 +0000

    work in progress

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from uuid import uuid4
 from datetime import datetime
-
+from models.__init__ import storage
 
 class BaseModel:
     """defines all common attributes/methods of other classes"""
@@ -22,6 +22,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def save(self):
         """
@@ -29,9 +30,11 @@ class BaseModel:
         with the current datetime
         """
         self.updated_at = self.created_at
+        models.storage.save()
 
     def __str__(self):
-        return ("[<class name>] (<self.id>) <self.__dict__>")
+        class_name = self.__class__.__name__
+        return (("[{}] ({}) {}".format(class_name, self.id, self.__dict__)))
 
     def to_dict(self):
         """returns a dictionary containing all keys/values
