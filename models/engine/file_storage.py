f0df73f77bb7bcb808cfe4ca6789443d820916a9
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-02-19 19:23:22 +0000

    trial

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,48 +1,114 @@
-#!/usr/bin/python3
+#!usr/bin/python3
+"""Defines the file storage class."""
+
+
 import json
 from models.base_model import BaseModel
 
 
 class FileStorage:
-    """serializes instances to a JSON file and
-       deserializes JSON file to instances
+
+    """Serializes an instance to JSON and
+
+    deserializes back to instance object
+
     """
+    __objects = {}
 
-    def __init__(self):
-        """Initializes"""
-        __file_path = "file.json"
-        __objects = {}
+    __file_path = "file.json"
 
-    def all(self):
-        """returns the dictionary __objects"""
+    def all(self, cls=None):
 
-        return FileStorage.__objects
+        """Returns a dictionary of __objects"""
+
+        return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
-        o_class_name = obj.__class__.__name__
-        FileStorage.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
+
+        """Sets in __ojbects the obj with key <obj class name >.id"""
+
+        if obj is not None:
+
+            # Concatenate class object name with id.
+
+            key = obj.__class__.__name__+"."+obj.id
+
+            # update the object dict with the new key and object as value.
+
+            self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
-        o_d = FileStorage.__objects
-        o_dic = {obj: o_d[obj].to_dict() for obj in o_d.keys()}
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(o_dic, f)
+
+        """" Serializes __objects to the JSON File(path:__file_path)"""
+
+        j_objects = {}
+
+        # For the python data to be serialized the data stored inside the objec
+
+        # variable as objects must be converted to a dictionary so that python
+
+        # for json to understand using to_dict method of the basemodel class
+
+        # The result of this will be a key with a unique dict as the value.
+
+        for key in self.__objects:
+
+            j_objects[key] = self.__objects[key].to_dict()
+
+        # Convert the dictionary into json and save in __filepath.
+
+        with open(self.__file_path, 'w') as f:
+
+            json.dump(j_objects, f)
 
     def reload(self):
+
         """
-        deserializes the JSON file to __objects
-        (only if the JSON file (__file_path) exists
-        ; otherwise, do nothing. If the file doesn’t exist,
-        no exception should be raised)
+
+        Deserializes the JSON file to objects
+
+        exists; otherwise does nothing, no exception should be raised
+
+        if the file does not exist.
+
         """
+
         try:
-            with open(FileStorage.__file_path) as f:
-                o_dic = json.load(f)
-                for o in o_dic.values():
-                    cls_name = o["__class__"]
-                    del o["__class__"]
-                    self.new(eval(cls_name)(**o))
-        except FileNotFoundError:
+
+            with open(self.__file_path, 'r', encoding="utf8") as f:
+
+                # The load method deserializes a json string into a python dict
+
+                obj_dict = json.load(f)
+
+            for obj_item in obj_dict.values():
+
+                # Extract the class from which to instantiate an object.
+
+                # Remember we loaded the values not the keys.
+
+                class_name = obj_item["__class__"]
+
+                # Since we are creating obj instances the attribute __class__
+
+                # should not be supplied as part of the arguments to the init
+
+                # function of the class(identified by class_name) all the other
+
+                # arguments are valid.
+
+                del obj_item["__class__"]
+
+                # Use the previously defined new function to create a new obj.
+
+                # The double asterics expands the dictionary to allow every key
+
+                # value pair from obj_item dict to be passed to the __init__()
+
+                # method of the class identified by class_name
+
+                self.new(eval(class_name)(**obj_item))
+
+        except:
+
             pass
